4d077a2891e8a0b24e800fe902b83b7e153f74a9
Thor <superpig119@qq.com>
2015-03-11 17:05:43 +1000

    mac test

--- onlineSearch/Feline.h
+++ onlineSearch/Feline.h
@@ -7,4 +7,5 @@ public:
 	void topologicalOrdering();
 	void DFSTravel(int &cnt, vector<int> &TopoSort);
 	void DFS(int sNum, vector<bool> &visited, int &time, vector<int> &vd, vector<int> &vf, vector<int> &vParent, int &cnt, vector<int> &TopoSort);
+
 };
