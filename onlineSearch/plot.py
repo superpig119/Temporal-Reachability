a4bdb33afca4f2324aabebc01c3f7cd5c22a52ec
Thor <superpig119@qq.com>
2015-03-15 22:41:56 +1000

    plot v0.2

--- onlineSearch/plot.py
+++ onlineSearch/plot.py
@@ -4,7 +4,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 file = open("coordinate")
-plt.plot([10, 20, 30])  
-plt.xlabel('tiems')  
-plt.ylabel('numbers')  
+lines = file.readlines()
+#plt.figure(1)
+ax1 = plt.subplot(111)
+for line in lines:
+    s = line.split();
+    l = s[1] + " " + s[2]
+    plt.plot(int(s[5]), int(s[6]), 'o', label = l)
+    plt.legend()
 plt.show()  
+
+#plt.plot([10, 20, 30])  
+#plt.xlabel('tiems')  
+#plt.ylabel('numbers')  
