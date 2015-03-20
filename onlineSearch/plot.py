a6b1cf5aa6f18d1c7dd1d76bc1e137dbac4fa934
superpig119 <superpig119@qq.com>
2015-03-20 16:38:34 +1000

    edge v3

--- onlineSearch/plot.py
+++ onlineSearch/plot.py
@@ -4,15 +4,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 coordinate = open("coordinate")
-lines = coordinate.readlines()
+edgefile = open("edges")
+nodes = coordinate.readlines()
+edges = edgefile.readlines()
 #plt.figure(1)
-nodes = {}
+nodeinfo = {}
 ax1 = plt.subplot(111)
-for line in lines:
-    s = line.split();
+for line in nodes:
+    s = line.split()
     l = s[1] + " " + s[2]
-	node[]
-    plt.plot(int(s[5]), int(s[6]), 'o', label = l)
+	nodeinfo[int(s[3])] = [int(s[1]), int(s[2])]
+	plt.plot(int(s[5]), int(s[6]), 'o', label = l)
 #    plt.legend()
+
+for edge in edges:
+	s = line.split()
+	plt.lines.line2D(nodeinfo[s[0]][0], nodeinfo[s[0]][0])
+
 plt.show()  
 
