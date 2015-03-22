511b56deeb19a04769f716c02e0309040f344cf0
Thor <superpig119@qq.com>
2015-03-22 22:57:53 +1000

    show edges finish

--- onlineSearch/plot.py
+++ onlineSearch/plot.py
@@ -11,21 +11,22 @@ nodes = coordinate.readlines()
 edges = edgefile.readlines()
 #plt.figure(1)
 nodeinfo = {}
-ax1 = plt.subplot(111)
+#ax1 = plt.subplot(111)
 for line in nodes:
     s = line.split()
     l = s[1] + " " + s[2]
-    nodeinfo[int(s[3])] = [int(s[1]), int(s[2])]
+    nodeinfo[int(s[3])] = [int(s[5]), int(s[6])]
     plt.plot(int(s[5]), int(s[6]), 'o', label = l)
 #    plt.legend()
 print nodeinfo
 
 for edge in edges:
-	s = edge.split()
-	print edge
-	print nodeinfo[int(s[1])], nodeinfo[int(s[2])]
-	l = Line2D(nodeinfo[int(s[0])], nodeinfo[int(s[1])])
-	plt.gca().add_line(l)
+    s = edge.split()
+    print edge
+    print s[0], nodeinfo[int(s[0])], s[1], nodeinfo[int(s[1])]
+#    plt.plot([1,3],[6,4])
+#l = Line2D(nodeinfo[int(s[0])], nodeinfo[int(s[1])])
+    plt.plot([nodeinfo[int(s[0])][0], nodeinfo[int(s[1])][0]], [nodeinfo[int(s[0])][1], nodeinfo[int(s[1])][1]])
 
 plt.show()  
 
