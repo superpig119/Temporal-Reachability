0f8df12d97e6f0156db7d739fc574829bb8a9dba
Thor <superpig119@qq.com>
2015-04-13 13:59:44 +1000

    new small world

--- data/simpleGraph/smallworld/n.py
+++ data/simpleGraph/smallworld/n.py
@@ -5,10 +5,10 @@ import networkx as nx
 #G=nx.star_graph(30)
 #nx.write_adjlist(G, "./star")
 
-#G=nx.watts_strogatz_graph(10000,20, 40)
+G=nx.watts_strogatz_graph(10000,20, 0.3)
 #nx.write_adjlist(G, "wssmall")
 
-G=nx.powerlaw_cluster_graph(10000,20,0.3)
+#G=nx.powerlaw_cluster_graph(100000,200,0.3)
 nx.write_adjlist(G, "powerlowc")
 
 
